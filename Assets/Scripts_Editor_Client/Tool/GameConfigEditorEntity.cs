bf5e811007f81b7e964947577ce5473017368e85
onovich <hjn1110@163.com>
2024-02-20 00:32:51 +0800

    <feature> fix: EM 读写二进制

--- Assets/Scripts_Editor_Client/Tool/GameConfigEditorEntity.cs
+++ Assets/Scripts_Editor_Client/Tool/GameConfigEditorEntity.cs
@@ -9,13 +9,14 @@ namespace Ping.Modifier {
     public class GameConfigEditorEntity : MonoBehaviour {
 
         [SerializeField] GameConfig clientTM;
-        [SerializeField] TextAsset serverTMBytes;
 
         const long len = 1024;
         byte[] buffer = new byte[len];
 
         [Button("Save")]
         public void Save() {
+
+            SaveDB();
 
         }
 
@@ -34,7 +35,7 @@ namespace Ping.Modifier {
             dbEntity.paddleMoveSpeedMax = clientTM.paddleMoveSpeedMax;
             dbEntity.paddleSize = new Ping.Server.Vector2(clientTM.paddleSize.x, clientTM.paddleSize.y);
 
-            int offset = 0; 
+            int offset = 0;
             dbEntity.WriteTo(buffer, ref offset);
             FileHelper.SaveBytes("GameConfig.bytes", buffer, offset);
 
@@ -43,14 +44,13 @@ namespace Ping.Modifier {
         [Button("Load")]
         public void Load() {
 
-            clientTM = LoadDB();
-
+            LoadDB();
             UnityEditor.EditorUtility.SetDirty(clientTM);
             AssetDatabase.SaveAssets();
 
         }
 
-        public GameConfig LoadDB() {
+        public void LoadDB() {
 
             if (FileHelper.Exists("GameConfig.bytes")) {
                 FileHelper.LoadBytes("GameConfig.bytes", buffer);
@@ -62,19 +62,19 @@ namespace Ping.Modifier {
             int offset = 0;
             dbEntity.FromBytes(buffer, ref offset);
 
-            GameConfig gameConfig = new GameConfig();
-            gameConfig.fieldBoundMax = new UnityEngine.Vector2(dbEntity.fieldBoundMax.x, dbEntity.fieldBoundMax.y);
-            gameConfig.fieldBoundMin = new UnityEngine.Vector2(dbEntity.fieldBoundMin.x, dbEntity.fieldBoundMin.y);
-            gameConfig.player1PaddleSpawnPos = new UnityEngine.Vector2(dbEntity.player1PaddleSpawnPos.x, dbEntity.player1PaddleSpawnPos.y);
-            gameConfig.player2PaddleSpawnPos = new UnityEngine.Vector2(dbEntity.player2PaddleSpawnPos.x, dbEntity.player2PaddleSpawnPos.y);
-            gameConfig.ballMoveSpeed = dbEntity.ballMoveSpeed;
-            gameConfig.ballMoveSpeedMax = dbEntity.ballMoveSpeedMax;
-            gameConfig.ballRadius = dbEntity.ballRadius;
-            gameConfig.ballSpawnAngleRange = dbEntity.ballSpawnAngleRange;
-            gameConfig.paddleMoveSpeed = dbEntity.paddleMoveSpeed;
-            gameConfig.paddleMoveSpeedMax = dbEntity.paddleMoveSpeedMax;
-            gameConfig.paddleSize = new UnityEngine.Vector2(dbEntity.paddleSize.x, dbEntity.paddleSize.y);
-            return gameConfig;
+            clientTM.fieldBoundMax = new UnityEngine.Vector2(dbEntity.fieldBoundMax.x, dbEntity.fieldBoundMax.y);
+            clientTM.fieldBoundMin = new UnityEngine.Vector2(dbEntity.fieldBoundMin.x, dbEntity.fieldBoundMin.y);
+            clientTM.player1PaddleSpawnPos = new UnityEngine.Vector2(dbEntity.player1PaddleSpawnPos.x, dbEntity.player1PaddleSpawnPos.y);
+            clientTM.player2PaddleSpawnPos = new UnityEngine.Vector2(dbEntity.player2PaddleSpawnPos.x, dbEntity.player2PaddleSpawnPos.y);
+            clientTM.ballMoveSpeed = dbEntity.ballMoveSpeed;
+            clientTM.ballMoveSpeedMax = dbEntity.ballMoveSpeedMax;
+            clientTM.ballRadius = dbEntity.ballRadius;
+            clientTM.ballSpawnAngleRange = dbEntity.ballSpawnAngleRange;
+            clientTM.paddleMoveSpeed = dbEntity.paddleMoveSpeed;
+            clientTM.paddleMoveSpeedMax = dbEntity.paddleMoveSpeedMax;
+            clientTM.paddleSize = new UnityEngine.Vector2(dbEntity.paddleSize.x, dbEntity.paddleSize.y);
+
+            PLog.Log("GameConfig loaded");
 
         }
 
